3c904b79c3a7078880d5d96a359ec40faf052743
rmc29 <rachael.churchill@cantab.net>
2025-04-09 18:30:04 +0100

    Filled in all answer option lists, including dates. Added modal animation.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const name = ["Ellie", "Harper", "Linda", "Frank", "Zara", "Andrew", "James", "Becky", "Ben", "Rose", "Dave", "Tom ", "Katie", "Doreen", "Naomi", "Chloe ", "Ruth", "Emma", "Edward", "Ashley", "Julia", "John", "Ali", "Rob ", "Stuart ", "Dawn", "Simon"]
-const noun = ["his parents", "her friend", "coffee beans", "the universe", "a coffee shop", "world peace", "the letter Q", "a duck", "potatoes", "an Easter card", "their house", "the road", "his car", "his job", "money", "existential terror", "an iPhone", "the Riemann hypothesis ", "cheesecake", "her sister", "her brother", "his sister", "his brother", "her parents", "a fruit smoothie", "a milkshake", "a cappuccino", "baked beans", "green beans", "jumping beans", "black beans"];
+const name = ["Ellie", "Harper", "Linda", "Frank", "Zara", "Andrew", "James", "Becky", "Ben", "Rose", "Dave", "Tom ", "Katie", "Doreen", "Naomi", "Chloe ", "Ruth", "Emma", "Edward", "Ashley", "Julia", "John", "Ali", "Rob ", "Stuart ", "Dawn", "Simon"].sort();
+const noun = ["his parents", "her friend", "coffee beans", "the universe", "a coffee shop", "world peace", "the letter Q", "a duck", "potatoes", "an Easter card", "their house", "the road", "his car", "his job", "money", "existential terror", "an iPhone", "the Riemann hypothesis ", "cheesecake", "her sister", "her brother", "his sister", "his brother", "her parents", "a fruit smoothie", "a milkshake", "a cappuccino", "baked beans", "green beans", "jumping beans", "black beans"].sort();
+const direction = ["north", "south", "east", "west", "ana", "kata", "rimward", "hubward", "downward", "upward"].sort();;
+const verb = ["visit", "deliver", "eat ", "drink", "kill", "defenestrate ", "seduce", "reverse-engineer", "buy", "sell ", "crash into", "analyse", "find", "apologise to", "drive ", "order", "fix", "break", "return", "transmute", "advertise"].sort();
+const month = Array.from({length: 13}, (value, index) => {
+  return new Date(0, index-1).toLocaleString('en-GB', {month: 'long'})
+});
+const date = Array.from({ length: 32 }, (value, index) => index);
+const year = Array.from({ length: 27},  (value, index) => 1999 + index);
 
 /* Question object:
 ID 
@@ -16,6 +23,9 @@ class Question {
 
 
 var questions = [
+new Question(date,2), 
+new Question(month,"February"),
+new Question(year, 2001),
 new Question(name,"James"), 
 new Question(name,"Linda"),
 new Question(noun, "his car")
@@ -52,7 +62,7 @@ function makeDropdown(optionstring, thisoption, index, array) {
 function populateDropdown(q, index) {
 	el = document.getElementById(index); //todo consider making the element IDs e.g. "q1" rather than "1"
 	console.log(q);
-	q.optionlist.sort();
+	//q.optionlist.sort();
 	optionstring = q.optionlist.reduce(makeDropdown);
 	
 	//for each option, concat  <option value="v">V</option>
