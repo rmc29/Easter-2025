7160b692f11a6a1fda7e94988393914985a280fa
rmc29 <rachael.churchill@cantab.net>
2025-04-15 14:13:11 +0100

    more photos, updates to lists etc

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-//todo get placeholder at start of lists
-const name = [".Name",  "Ellie", "Harper", "Linda", "Frank", "Zara", "Andrew", "James", "Becky", "Ben", "Rose", "Dave", "Tom ", "Katie", "Doreen", "Naomi", "Chloe ", "Ruth", "Emma", "Edward", "Ashley", "Julia", "John", "Ali", "Rob ", "Stuart ", "Dawn", "Simon"].sort();
-const noun = [".noun", "his parents", "her friend", "coffee beans", "the universe", "a coffee shop", "world peace", "the letter Q", "a duck", "potatoes", "an Easter card", "their house", "the road", "his car", "his job", "money", "existential terror", "an iPhone", "the Riemann hypothesis ", "cheesecake", "her sister", "her brother", "his sister", "his brother", "her parents", "a fruit smoothie", "a milkshake", "a cappuccino", "baked beans", "green beans", "jumping beans", "black beans", ].sort();
-const direction = ["north", "south", "east", "west", "ana", "kata", "rimward", "hubward", "downward", "upward"].sort();;
-const verb = ["visit", "deliver", "eat", "drink", "kill", "defenestrate", "seduce", "reverse-engineer", "buy", "sell", "crash into", "analyse", "find", "apologise to", "drive", "order", "fix", "break", "return", "transmute", "advertise"].sort();
-const verbed = ["visited", "delivered", "ate", "drank", "killed", "defenestrated", "seduced", "reverse-engineered", "bought", "sold", "crashed into", "analysed", "found", "apologised to", "drove", "ordered", "fixed", "broke", "returned", "transmuted", "advertised"].sort();
-const verbing = ["visiting", "delivering", "eating", "drinking", "killing", "defenestrating", "seducing", "reverse-engineering", "buying", "selling", "crashing into", "analysing", "finding", "apologising to", "driving", "ordering", "fixing", "breaking", "returning", "transmuting", "advertising"].sort();
+const name = ["(Name)",  "Ellie", "Harper", "Linda", "Frank", "Zara", "Andrew", "James", "Becky", "Ben", "Rose", "Dave", "Tom ", "Katie", "Doreen", "Naomi", "Chloe ", "Ruth", "Emma", "Edward", "Ashley", "Julia", "John", "Ali", "Rob ", "Stuart ", "Dawn", "Simon"].sort();
+const noun = ["(noun)", "his parents", "her friend", "coffee beans", "the universe", "a coffee shop", "world peace", "the letter Q", "a duck", "potatoes", "an Easter card", "their house", "the road", "his car", "his job", "money", "existential terror", "an iPhone", "the Riemann hypothesis ", "cheesecake", "her sister", "her brother", "his sister", "his brother", "her parents", "a fruit smoothie", "a milkshake", "a cappuccino", "baked beans", "green beans", "jumping beans", "black beans", ].sort();
+const direction = ["(direction)","north", "south", "east", "west", "downward", "upward","ana", "kata", "rimward", "hubward" ];
+const verb = ["(verb)","visit", "deliver", "eat", "drink", "kill", "defenestrate", "seduce", "reverse-engineer", "buy", "sell", "crash into", "analyse", "find", "apologise to", "drive", "order", "fix", "break", "return", "transmute", "advertise"].sort();
+const verbed = ["(verbed)","visited", "delivered", "ate", "drank", "killed", "defenestrated", "seduced", "reverse-engineered", "bought", "sold", "crashed into", "analysed", "found", "apologised to", "drove", "ordered", "fixed", "broke", "returned", "transmuted", "advertised"].sort();
+const verbing = ["(verbing)","visiting", "delivering", "eating", "drinking", "killing", "defenestrating", "seducing", "reverse-engineering", "buying", "selling", "crashing into", "analysing", "finding", "apologising to", "driving", "ordering", "fixing", "breaking", "returning", "transmuting", "advertising"].sort();
 const month = Array.from({length: 12}, (value, index) => {
   return new Date(0, index).toLocaleString('en-GB', {month: 'long'})
 });
+month.unshift("(Month)");
 const date = Array.from({ length: 31 }, (value, index) => index+1);
+date.unshift("(Date)");
 const year = Array.from({ length: 26},  (value, index) => 2000 + index);
+year.unshift("(Year)");
 const hour = Array.from({ length: 12 }, (value, index) => index);
-const day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+hour.unshift("(Hour)");
+const day = ["(Weekday)", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 
 //const formText = "On 0 1 2, 3 crashed into 4 with 5."
@@ -19,7 +22,7 @@ const day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",
 On Saturday the 7th April, 2012, at about 5pm, Andrew Harper was driving north, to visit his parents, and crashed into Dave Stuart, who was driving south from Woodley to Bellington, to deliver coffee beans.<br>
 Meanwhile, Becky was chatting to her friend Zara, and Ellie was at a coffee shop in X, where she ordered a fruit smoothie from Geoff the barista." */
 
-const formText = "0 1 and her husband 2 have four grown-up children, who are, in birth order, 3, 4, 5 and 6.<br> On 7 8 9, 10, at about 11 pm, 12 13 was 14 15, to 16 17, and 18 19 20, who was 21 22 from 23 to 24, to 25 26.<br> Meanwhile, 27 was 28 29 30, and 31 was at 32 in 33, where she had 34 35 from 36 the barista."
+const formText = "0 1 and her husband 2 have four grown-up children, who are, in birth order, 3, 4, 5 and 6.<br><BR> On 7 8 9, 10, at about 11 pm, 12 13 was 14 15, to 16 17, and 18 19 20, who was 21 22 from 23 to 24, to 25 26.<br><BR> Meanwhile, 27 was 28 29 30, and 31 was at 32 in 33, where she 34 35 from 36 the barista."
 
 const locations = ["indoor", "outdoor", "road", "coffeeshop", /*"upstairs"*/];
 
@@ -113,13 +116,15 @@ function hide(id) {
 }
 /*called for each answer in a question's answer list*/
 function makeDropdown(optionstring, thisoption, index, array) {
-	console.log(optionstring, thisoption);
+	//console.log(optionstring, thisoption);
 	return optionstring + "<option value='" + thisoption + "'>" + thisoption + "</option>"; 
+	//return optionstring + "<option value='" + thisoption + "' style='color:" + (index ? "black" : "gray") + "'>" + thisoption + "</option>"; 
+	//return optionstring + "<option " + (index ? "" : "disabled selected") + " value='" + thisoption + "'>" + thisoption + "</option>"; 
 }
 
 function populateDropdown(q, index) {
 	el = document.getElementById(index); //todo consider making the element IDs e.g. "q1" rather than "1"
-	console.log(q);
+	//console.log(q);
 	//q.optionlist.sort();
 	optionstring =  "";
 	optionstring = q.optionlist.reduce(makeDropdown, optionstring);
@@ -133,10 +138,15 @@ function populateDropdown(q, index) {
 function populateForm() {
 	
 	//On <select id="0" onchange="checkAnswers();"></select> <select id="1" onchange="checkAnswers();"></select> <select id="2" onchange="checkAnswers();"></select>,
-	t = formText.replace(/([0-9]+)/g, "<select id='$1' onchange='checkAnswers();'></select>");
+	t = formText.replace(/([0-9]+)/g, "<select id='$1' class='rounded border-2 border-red-600 shadow-md' onchange='checkAnswers();'></select>");
 	//console.log(t);
 	document.getElementById("form").innerHTML = t;
 	questions.forEach(populateDropdown);
+}
+
+function makeSelectElementGreen(value) {
+	console.log(value);
+	value.class = 'rounded border-2 border-green-600 shadow-md';
 }
 
 function isAnswerCorrect(value, index) {
@@ -144,13 +154,21 @@ function isAnswerCorrect(value, index) {
 }
 
 function checkAnswers() {
-	console.log("check");
-	if (questions.every(isAnswerCorrect)) console.log("correct!")
+	//console.log("check");
+
+	if (questions.every(isAnswerCorrect) || tmp==1) {
+		console.log("correct!")
+		document.getElementById("notes").class = "rounded-lg border-4 border-green-600 shadow-lg";
+		document.getElementById("form-content").class = "modal-content; rounded-xl border-4 border-green-600 bg-gradient-to-tr from-green-500 to-emerald-500" ;
+		document.getElementsByTagName("select").forEach(makeSelectElementGreen);
+		
+	}
 }
 
 function start() {
 	 detectMob();
 	 populateForm();
+	 	tmp=0;
 }
 
 
