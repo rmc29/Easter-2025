1f8418234bef7f60db86fff6f4e54060aed4db7a
rmc29 <rachael.churchill@cantab.net>
2025-04-18 08:39:02 +0100

    Updated convo about Andrew. Made modals close when you click outside them. General tidying and adding more comments.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const name = ["(Name)", "Ellie", "Harper", "Linda", "Frank", "Zara", "Andrew", "James", "Becky", "Ben", "Rose", "Dave", "Tom ", "Katie", "Doreen", "Naomi", "Chloe", "Ruth", "Emma", "Ed", "Ashton", "Julia", "John", "Ali", "Rob", "Stuart", "Dawn", "Simon", "Geoff", "Sarah"].sort();
+// Lists of all possible verbs, nouns, etc
+const name = ["(Name)", "Ellie", "Harper", "Linda", "Frank", "Zara", "Andrew", "James", "Becky", "Ben", "Rose", "Dave", "Tom ", "Katie", "Doreen", "Naomi", "Chloe", "Ruth", "Emma", "Ed", "Ashton", "Julia", "John", "Ali", "Rob", "Stuart", "Dawn", "Simon", "Geoff", "Sarah", "Doreen"].sort();
 const noun = ["(noun)", "his parents", "her friend", "coffee beans", "the universe", "a coffee shop", "world peace", "the letter Q", "a duck", "potatoes", "an Easter card", "their house", "the road", "his car", "his job", "money", "existential terror", "an iPhone", "the Riemann hypothesis ", "cheesecake", "her sister", "her brother", "his sister", "his brother", "her parents", "a fruit smoothie", "a milkshake", "a cappuccino", "baked beans", "green beans", "jumping beans", "black beans"].sort();
 const place = ["(place)", "Woodley", "Bellington", "Farley", "Norton", "Harpton", "Mereham" ].sort();
 const direction = ["(direction)","north", "south", "east", "west", "downward", "upward","ana", "kata", "rimward", "hubward" ];
@@ -18,14 +19,11 @@ hour.unshift("(Hour)");
 const day = ["(Weekday)", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 
-/* const formText = "Linda Harper and her husband Frank have four grown-up children, who are, in birth order, Ellie, X, X and Becky.<br>
-On Saturday the 7th April, 2012, at about 5pm, Andrew Harper was driving north, to visit his parents, and crashed into Dave Stuart, who was driving south from Woodley to Bellington, to deliver coffee beans.<br>
-Meanwhile, Becky was chatting to her friend Zara, and Ellie was at a coffee shop in X, where she ordered a fruit smoothie from Geoff the barista." */
 
 const formText = [
 "0 1 and her husband 2 have four grown-up children, who are, in birth order, 3, 4, 5 and 6.",
 "On 0 1 2, 3, at about 4 pm, 5 6 was 7 8, to 9 10, and 11 12 13, who was 14 15 from 16 to 17, to 18 19.",
- "Meanwhile, 0 (and her mother) were 1 2 3, and 4 was at 5 in 6, where she 7 8 from 9 the barista.",
+ "Meanwhile, 0 (and her mother) were 1 2 3, and 4 was at 5 in 6, 7 8 from 9 the barista.",
  ];
 
 const locations = ["indoor", "outdoor", "road", "coffeeshop", "lounge"];
@@ -41,7 +39,7 @@ class Question {
 	}
 
 }
-
+//Questions to go on the form, in a nested array: 3 sub-forms, each containing multiple questions
 var questions = [
 
 [
@@ -88,13 +86,25 @@ new Question(name,"Zara"),
 new Question(name,"Ellie"),
 new Question(noun, "a coffee shop"),
 new Question(place, "Bellington"), 
-new Question(verbed, "ordered"),
+new Question(verbing, "ordering"),
 new Question(noun, "a fruit smoothie"),
 new Question(name, "Geoff")
 ]
 ]
 
+// Utility functions
 
+//To close modals if the user clicks on the grey overlay outside them
+window.onclick = function(event) {
+  classlist = Array.from(event.target.classList);
+ 
+  if (classlist[0] == "modal") {
+    event.target.style.display = "none";
+  }
+
+}
+
+//to change location: display target location, hide all others
 function go(targetLocation) { 
 	locations.forEach(hide); 
 	display(targetLocation);
@@ -118,28 +128,28 @@ function hide(id) {
 		alert("Happy Easter! I recommend viewing this on desktop because it's built in old-school JavaScript without any of that newfangled responsive design stuff.");
 	}
 }
-/*called for each answer in a question's answer list*/
-function makeDropdown(optionstring, thisoption) {
-	//console.log(optionstring, thisoption);
+
+// Functions for generating the answer form and its dropdowns
+
+//called to generate the HTML for each answer in a question's answer list
+function makeDropdownOption(optionstring, thisoption) {
 	return optionstring + "<option value='" + thisoption + "'>" + thisoption + "</option>"; 
 
 }
 
 function populateDropdown(q, index) {
 	id = this + "-" + index;
-	//console.log(id);
 	el = document.getElementById(id); 
 	optionstring =  "";
-	optionstring = q.optionlist.reduce(makeDropdown, optionstring);
+	optionstring = q.optionlist.reduce(makeDropdownOption, optionstring);
 	
 	//for each option, concat  <option value="v">V</option>
-	//if user already selected an answer, <option value="v" selected>V</option>
 	el.innerHTML += optionstring;
 }
 
 
 function populateForm() {
-	
+	//From the form text (text with numbers in, like "On 0 1 2,...") generate text with HTML <select> elements in for each question, like this:
 	//On <select id="0" onchange="checkAnswers();"></select> <select id="1" onchange="checkAnswers();"></select> <select id="2" onchange="checkAnswers();"></select>,
 	for (i in [0,1,2]) {
 		t = "<div class='answerform incorrect' id='form" + i + "'>";
@@ -147,15 +157,13 @@ function populateForm() {
 		t += "</div>";
 		document.getElementById("form").innerHTML += t;
 		questions[i].forEach(populateDropdown, i);
-		//console.log(t);
-
 	}
-	//questions.flat().forEach(populateDropdown);
 	
 }
 
+//Functions for checking answers and changing various formatting if a whole (sub-)form is correct
+
 function isAnswerCorrect(question, index) {
-		//console.log(question + " " + this);
 		return (document.getElementById(this + "-" + index).value == question.correctAnswer);
 		
 }
@@ -163,12 +171,11 @@ function isAnswerCorrect(question, index) {
 //Add the "correct" class to an element, which styles it green	
 function addCorrectClass(el) {
 	
-	//console.log(el);
 	el.classList.add("correct");
-	el.classList.add("greentext");
-	
+	el.classList.add("greentext");	
 }
-//add disabled attribute - locking the correct answers in
+
+//Add disabled attribute - locking the correct answers in
 function lockIn(el) {
 	
 	//console.log(el);
@@ -199,6 +206,8 @@ function checkAnswers() {
 	display("credits-modal");
 	}
 }
+
+// Start and finish functions
 
 function start() {
 	 detectMob();
